d9eb22cb309c8dc381252e8239f2057213b96283
Abhinav Kumar <114800265+abhinav900@users.noreply.github.com>
2022-12-01 23:28:20 +0530

    KOC28_DAY-3 Projects

--- project05.py
+++
@@ -1,6 +0,0 @@
-#String Formatting in Python
-name="Priyansh"
-age=18
-print("Hello "+name+" your age is "+str(age)) #Ugly String Formatting
-
-print("Hello %s your age is %d"%(name,age)) #Better String Formatting
